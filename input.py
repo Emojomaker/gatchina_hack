a5dd6a5954714b828c90d40e4185052f138a2f91
Emojomaker <p.kraskin@outlook.com>
2019-06-13 21:46:19 +0300

    Update input.py

--- input.py
+++ input.py
@@ -9,9 +9,9 @@ import requests
 os.environ["NLS_LANG"] = "American_America.AL32UTF8"
 
 cloudinary.config(
-  cloud_name = 'ddu7z1mp6',
-  api_key = '318623113898338',
-  api_secret = 'oVhoLqjYaCNuzL-hfsYoy0eEvqs'
+  cloud_name = '*****',
+  api_key = '*****',
+  api_secret = '*****'
 )
 
 app = Flask(__name__)
@@ -30,7 +30,7 @@ def get_photo(link):
 
 def tranclate_desc(text):
     text_to_translate = text
-    key = 'trnsl.1.1.20190526T102718Z.3d3b72d7b2c9b3b8.f80451c53e3af46685221375aef4feb378cb99fb'
+    key = '*****'
     language = 'en-ru'
     link = 'https://translate.yandex.net/api/v1.5/tr.json/translate?lang='+language+'&key='+key+'&text='+text_to_translate
     info = requests.post(url=link)
@@ -51,12 +51,11 @@ def create_incident():
         'description_on_photo': request.json['description_on_photo']
     }
     translated_text = (tranclate_desc(incident['description_on_photo']))
-    conn = cx_Oracle.connect('hack','123456','192.168.137.49:1521/gdb')
+    conn = cx_Oracle.connect('*****','*****','*****')
     cur = conn.cursor()
     cur.execute('SELECT MAX(id)+1 from hack.input')
     sequence = cur.fetchone()
     sequence = str(sequence).strip('(,)')
-    #sq = "INSERT INTO HACK.INPUT (ID, DESCRIPTION, CDATE, ATTACH, USER_ID, GPS, PRIORITY, SPEECH_TO_TEXT) VALUES ('"+sequence+"','"+translated_text+"',(to_date('"+incident['date']+"','DD.MM.YYYY hh24:mi:ss')),'"+incident['attach']+"','"+incident['user_id']+"','"+incident['gps']+"','"+incident['priority']+"','"+incident['speech_to_text']
     cur.execute("INSERT INTO HACK.INPUT (ID, DESCRIPTION, CDATE, ATTACH, USER_ID, GPS, PRIORITY, SPEECH_TO_TEXT) VALUES ('"+sequence+"','"+translated_text+"',(to_date('"+incident['date']+"','DD.MM.YYYY hh24:mi:ss')),'"+incident['attach']+"','"+incident['user_id']+"','"+incident['gps']+"','"+incident['priority']+"','"+incident['speech_to_text']+"')")
     conn.commit()
     cur.close()
@@ -70,7 +69,7 @@ def create_incident():
 
 @app.route('/incidents/api/v1.0/get_incidents', methods=['GET'])
 def get_incident():
-    conn = cx_Oracle.connect('hack','123456','192.168.137.49:1521/gdb')
+    conn = cx_Oracle.connect('*****','*****','*****')
     cur = conn.cursor()
     cur.execute('SELECT CDATE, USER_ID, GPS, SPEECH_TO_TEXT FROM HACK.INCIDENT')
     return jsonify(cur.fetchall()), 201, {'Content-Type': 'text/css; charset=utf-8'}
@@ -79,7 +78,7 @@ def get_incident():
 
 @app.route('/incidents/api/v1.0/get', methods=['GET'])
 def get():
-    conn = cx_Oracle.connect('hack','123456','192.168.137.49:1521/gdb')
+    conn = cx_Oracle.connect('*****','*****','*****')
     cur = conn.cursor()
     cur.execute('SELECT CDATE, USER_ID, GPS, SPEECH_TO_TEXT FROM HACK.INCIDENT')
     res = cur.fetchall()
